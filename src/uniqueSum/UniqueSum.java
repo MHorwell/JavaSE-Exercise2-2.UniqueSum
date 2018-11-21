1a153feb345e757024c1e9afafd5309d835a6350
MHorwell <mattjhorwell@gmail.com>
2018-11-21 11:16:01 +0000

    Completed Unique Sum Exercise to return the sum of unique integers when given three.

---
+++ src/uniqueSum/UniqueSum.java
@@ -0,0 +1,21 @@
+package uniqueSum;
+
+public class UniqueSum {
+	
+		public static void main(String[] args) {
+			System.out.println(addUniqueNumbers(2, 4, 4));
+		}
+		
+		public static int addUniqueNumbers(int numberOne, int numberTwo, int numberThree) {
+		if (numberOne == numberTwo || numberOne == numberThree){
+			numberOne = 0;
+		}
+		if (numberTwo == numberThree){
+			numberTwo = 0;
+		}
+		return numberOne + numberTwo + numberThree;
+		}
+
+	}
+
+
