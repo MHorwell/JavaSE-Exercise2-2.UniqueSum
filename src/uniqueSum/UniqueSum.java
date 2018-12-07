84af8bb080c0f4e6b2a47f2fb5247a16cc6e8f59
MHorwell <mattjhorwell@gmail.com>
2018-12-07 17:17:37 +0000

    Minor fix implemented

--- src/uniqueSum/UniqueSum.java
+++ src/uniqueSum/UniqueSum.java
@@ -1,21 +1,19 @@
 package uniqueSum;
 
 public class UniqueSum {
-	
-		public static void main(String[] args) {
-			System.out.println(addUniqueNumbers(2, 4, 4));
-		}
-		
-		public static int addUniqueNumbers(int numberOne, int numberTwo, int numberThree) {
-		if (numberOne == numberTwo || numberOne == numberThree){
+
+	public static void main(String[] args) {
+		System.out.println(addUniqueNumbers(2, 4, 5));
+	}
+
+	public static int addUniqueNumbers(int numberOne, int numberTwo, int numberThree) {
+		if (numberOne == numberTwo || numberOne == numberThree) {
 			numberOne = 0;
 		}
-		if (numberTwo == numberThree){
+		if (numberTwo == numberThree) {
 			numberTwo = 0;
 		}
 		return numberOne + numberTwo + numberThree;
-		}
-
 	}
 
-
+}
